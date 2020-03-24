dbdb5c81f428f0fc2276a6999705bdc0b7ae01d7
Dahveyea <dahveyea.cowan@gmail.com>
2020-03-24 12:38:09 -0700

    Added ports to help scan

--- Portscanner.py
+++ Portscanner.py
@@ -1,9 +1,28 @@
 import socket
+import sys
+import threading
+import queue
+import time
+
+
+
+common_ports = {
+    "21": "FTP",
+    "22": "SSH",
+    "23": "Telnet",
+    "25": "SMTP",
+    "53": "DNS",
+    "80": "HTTP",
+    "194": "IRC",
+    "443": "HTTPS",
+    "3306": "MySQL",
+    "25565": "Minecraft"
+}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(5)
 
-host = input("PLease enter the IP you want to scan: ")
+host = input("Please enter the IP you want to scan: ")
 port = int(input("Please enter the port you want to scan: "))
 
 def portscanner(port):
