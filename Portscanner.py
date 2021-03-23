c50dc3e0d543c4b705ed0557a628929cdafa210c
Dahveyea <dahveyea.cowan@gmail.com>
2021-03-23 00:00:01 -0700

    Adding finishing touches

--- Portscanner.py
+++ Portscanner.py
@@ -5,7 +5,7 @@ import queue
 import time
 import os
 import platform
-from socket import *
+# from socket import *
 
 
 # common_ports = {
@@ -18,39 +18,69 @@ from socket import *
 #     "194": "IRC",
 #     "443": "HTTPS",
 # }
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.settimeout(5)
-#
-# host = input("Please enter the IP you want to scan: ")
-# port = int(input("Please enter the port you want to scan: "))
-#
-# def portscanner(port):
-#     if s.connect_ex((host, port)):
-#         print("The port is closed")
-#     else:
-#         print("The port is open")
-# portscanner(port)
-
-
-# startTime = time.time()
-#
-# if __name__ == '__main__':
-#    target = input('Enter the host to be scanned: ')
-#    t_IP = gethostbyname(target)
-#    print ('Starting scan on host: ', t_IP)
-#
-#    for i in range(50, 500):
-#       s = socket(AF_INET, SOCK_STREAM)
-#
-#       conn = s.connect_ex((t_IP, i))
-#       if(conn == 0) :
-#          print ('Port %d: OPEN' % (i,))
-#       s.close()
-# print('Time taken:', time.time() - startTime)
 
 
 
+startTime = time.time()
+
+if __name__ == '__main__':
+   target = input('Enter the host to be scanned: ')
+   t_IP = gethostbyname(target)
+   print ('Starting scan on host: ', t_IP)
+
+   for i in range(1, 500):
+      s = socket(AF_INET, SOCK_STREAM)
+
+      conn = s.connect_ex((t_IP, i))
+      if(conn == 0) :
+         print ('Port %d: OPEN' % (i,))
+      s.close()
+print('Time taken:', time.time() - startTime)
+
+
+#Threading to make the original port scanner faster
+from queue import Queue
+socket.setdefaulttimeout(0.25)
+print_lock = threading.Lock()
+
+target = input('Enter the host to be scanned: ')
+t_IP = socket.gethostbyname(target)
+print ('Starting scan on host: ', t_IP)
+
+def portscan(port):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      con = s.connect((t_IP, port))
+      with print_lock:
+         print(port, 'is open')
+      con.close()
+   except:
+      pass
+
+def threader():
+   while True:
+      worker = q.get()
+      portscan(worker)
+      q.task_done()
+
+q = Queue()
+   startTime = time.time()
+
+for x in range(100):
+   t = threading.Thread(target = threader)
+   t.daemon = True
+   t.start()
+
+for worker in range(1, 500):
+   q.put(worker)
+
+q.join()
+print('Time taken:', time.time() - startTime)
+
+
+
+
+#The ping sweep function
 from datetime import datetime
 net = input("Enter the Network Address: ")
 net1= net.split('.')
@@ -85,3 +115,37 @@ for ip in range(st1,en1):
 t2 = datetime.now()
 total = t2 - t1
 print ("Scanning completed in: ",total)
+
+
+
+#This is the TCP Scan function incase Ping sweep doesnt work
+from datetime import datetime
+net = input("Enter the IP address: ")
+net1 = net.split('.')
+a = '.'
+
+net2 = net1[0] + a + net1[1] + a + net1[2] + a
+st1 = int(input("Enter the Starting Number: "))
+en1 = int(input("Enter the Last Number: "))
+en1 = en1 + 1
+t1 = datetime.now()
+
+def scan(addr):
+   s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+   socket.setdefaulttimeout(1)
+   result = s.connect_ex((addr,80))
+   if result == 0:
+      return 1
+   else :
+      return 0
+
+def run1():
+   for ip in range(st1,en1):
+      addr = net2 + str(ip)
+      if (scan(addr)):
+         print (addr , "is live")
+
+run1()
+t2 = datetime.now()
+total = t2 - t1
+print ("Scanning completed in: " , total)
